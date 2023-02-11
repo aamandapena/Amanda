80ad9d8900855eaa9c3cbf320a213c3878419ea0
aamandapena <115810907+aamandapena@users.noreply.github.com>
2023-02-10 21:57:11 -0300

    Rename Lista 3_A,mandaPena.R to Lista 3_AmandaPena.R

--- Lista 3_AmandaPena.R
+++ Lista 3_AmandaPena.R
@@ -1,10 +1,10 @@
 ##LISTA 3 DE ECONOMETRIA II##
 ##AMANDA PENA##
 
-##DEFININDO DIRETÓRIO DE TRABALHO##
+##DEFININDO DIRETÃ“RIO DE TRABALHO##
 
 
-##QUESTÃO 1##
+##QUESTÃƒO 1##
 
 ##Carregando pacotes##
 library(ggplot2)
@@ -19,9 +19,9 @@ library(urca)
 
 ##Verificando a base de dados##
 par(mfrow=c(3,1))
-plot.ts(inf, main="Inflação")
-stats::acf(inf,lag.max=50 , main="ACF Inflação")
-stats::pacf(inf,lag.max=50 , main="PACF Inflação")
+plot.ts(inf, main="InflaÃ§Ã£o")
+stats::acf(inf,lag.max=50 , main="ACF InflaÃ§Ã£o")
+stats::pacf(inf,lag.max=50 , main="PACF InflaÃ§Ã£o")
 plot.ts(juros, main= "Juros")
 acf(juros,lag.max=50, main= "ACF Juros")
 stats::pacf(juros,lag.max=50 , main="PACF Juros")
@@ -31,20 +31,20 @@ stats::pacf(y,lag.max=50 , main="PACF Produto")
 
 ##LETRA A##
 
-##Verificando Estacionareidade das séries## dúvida*
+##Verificando Estacionareidade das sÃ©ries## dÃºvida*
 
-##Transformações das séries que precisam estacionareizar##
+##TransformaÃ§Ãµes das sÃ©ries que precisam estacionareizar##
 par(mfrow=c(3,1))
 dinf = diff(inf)
-plot.ts(dinf, main="Inflação com Primeira Diferença")
-stats::acf(dinf,lag.max=50 , main="ACF Inflação com Primeira Diferença")
-stats::pacf(dinf,lag.max=50 , main="PACF Inflação com Primeira Diferença")
+plot.ts(dinf, main="InflaÃ§Ã£o com Primeira DiferenÃ§a")
+stats::acf(dinf,lag.max=50 , main="ACF InflaÃ§Ã£o com Primeira DiferenÃ§a")
+stats::pacf(dinf,lag.max=50 , main="PACF InflaÃ§Ã£o com Primeira DiferenÃ§a")
 djuros = diff(juros)
-plot.ts(djuros, main="Juros com Primeira Diferença")
-stats::acf(djuros,lag.max=50 , main="ACF Juros com Primeira Diferença")
-stats::pacf(djuros,lag.max=50 , main="PACF Juros com Primeira Diferença")
+plot.ts(djuros, main="Juros com Primeira DiferenÃ§a")
+stats::acf(djuros,lag.max=50 , main="ACF Juros com Primeira DiferenÃ§a")
+stats::pacf(djuros,lag.max=50 , main="PACF Juros com Primeira DiferenÃ§a")
 
-##Juntar séries
+##Juntar sÃ©ries
 data <- window(ts.intersect(y, dinf, djuros),
                start=c(2000,01))
 colnames(data) <- c('produto', 'inflacao', 'juros')
@@ -65,11 +65,11 @@ plot.ts(res)
 
 ################################################################################
 
-##QUESTÃO 2##
+##QUESTÃƒO 2##
 
 options(OutDec=".") # trocando ponto por v?rgula
 
-dados=read.table("clipboard",head=T) ##até 12/2012
+dados=read.table("clipboard",head=T) ##atÃ© 12/2012
 class(dados)
 dados
 plot.ts(log(dados))
